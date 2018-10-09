2e436f04e1ae1c980abf81a53b69055f4d088b86
ZhangQiangQQ <1562055324@qq.com>
2018-10-09 11:11:59 +0800

    uodate&delete

--- miniprogram/pages/bookDetail/bookDetail.js
+++ miniprogram/pages/bookDetail/bookDetail.js
@@ -1,3 +1,4 @@
+// pages/bookDetail/bookDetail.js
 const db = wx.cloud.database()
 const books = db.collection('mybooks')
 Page({
@@ -6,75 +7,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-    booklist:[]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    db.collection('mybooks').get().then(res => {
-      // res.data 包含该记录的数据
+    console.log(options)
+    db.collection('mybooks').doc(options.id).get().then(res => {
       this.setData({
-        booklist:res.data
+        bookDetail:res.data,
+        bookDetail_id: options.id
       })
     })
-    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
-  _bookDetail: (item) => {
-    let current_item_id = item.currentTarget.dataset.id
-    // console.log(current_item_id)
-    wx.navigateTo({
-      url: '../bookDetail/bookDetail?id=' + current_item_id 
+  updates: (event) => {
+    db.collection('mybooks').doc(event.currentTarget.dataset.id).update({
+      // data 传入需要局部更新的数据
+      data: {
+        title: "更新测试3"
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: (err)=>{
+        console.log(err)
+      }
+    })
+  },
+  deletes : (event) => {
+    db.collection('mybooks').doc(event.currentTarget.dataset.id).remove({
+      success: function (res) {
+        console.log(res)
+      }
     })
   }
 })
